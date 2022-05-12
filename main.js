ed7fd7971d47df7bf5e831ffdb96a5b94efe9382
TheYingineer <ying.yan.huang87@gmail.com>
2022-05-11 22:39:15 -0500

    ready to submit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assert = require('assert');
+const console = require('console');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -27,14 +28,65 @@ const generateSolution = () =>  {
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+/**
+ * Spec 2.1 - Split up the solution and guess: In generateHint(), create variables solutionArray and guessArray that each split up passed in arguments, [.split](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/splitting on '' (empty string).
+Spec 2.2 - Determine correct "letter-locations": Create a variable correctLetterLocations and set it to 0. This variable will record how many correct "letter-locations" were guessed. For instance, a guess of aabc against a solution of deba would yield one correct "letter-location" (b). In a for loop, iterate over the solutionArray, comparing each index of solutionArray against the same index of guessArray. If the item matches, increment correctLetterLocations, and set that index in solutionArray to null.
+ */
+const generateHint = (guess) =>  {
+  let solutionArray = solution.split("")
+  let guessArray = guess.split("")
+  console.log(solutionArray)
+  console.log(guessArray)
+  let correctLetterLocations = 0
+  for(let i=0; i<solutionArray.length; i++) {
+    if(solutionArray[i] === guessArray[i]){
+      correctLetterLocations = correctLetterLocations + 1
+      solutionArray[i] = 0
+      guessArray[i] = 1
+    }
+  }
 
-const generateHint = () =>  {
-  // your code here
+  // Spec 2.3 - Determine correct "letters": Now that we have nulled the already counted correctLetterLocations, we can see if the guessArray contains any correctLetters that were not in the correct location. Set a variable correctLetters equal to 0, and in a for loop, again iterate over the solutionArray. Using .indexOf, determine if the item at the current index in guessArray appears inside of solutionArray. Save that index in a variable called targetIndex. Now, if targetIndex is greater than -1(it exists in the array), increment correctLetters and set the item in solutionArray at that index equal to null.
+
+  // Spec 2.4 - return hint string: Optionally, you can use the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen. > (NOTE: If you choose to use this color package, only console.log the result. If you return the result your program will fail the tests.)
+
+  let correctLetters = 0
+for(let j = 0; j<guessArray.length; j++){
+  let targetIndex = guessArray.indexOf(solutionArray[j])
+  if(targetIndex>-1){
+    correctLetters = correctLetters+1
+
+    //mutate both solution and guess array so that handle duplicates properly
+    solutionArray[j] = 0 //Ceaser
+    guessArray[targetIndex] = 1 //Ceaser
+  }
+}
+return correctLetterLocations + "-" + correctLetters
 }
 
+// Spec 3 - Add guess and hint to the board: Define a variable called hint that collects the returned value of generateHint(guess). .push the guess and the hint (as a combined string) into the board.
+
+// Spec 4 - End the game: After 10 incorrect guesses, if the board length equals 10, return 'You ran out of turns! The solution was' and the solution. Otherwise, return 'Guess again.'.
+
+
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  solution = 'dced';
+
+  if(board.length === 10){
+    return console.log( "You ran out of turns! The solution was " + solution)
+  }
+
+
+  if(guess === solution){
+    return "You guessed it!"
+  }
+  else {
+    let hint = generateHint(guess)
+    board.push(guess + " " + hint)
+    console.log("Guess again.")
+    return hint
+
+  }
 }
 
 
@@ -65,7 +117,7 @@ if (typeof describe === 'function') {
       assert.equal(generateHint('abdc'), '2-2');
     });
     it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+      assert.equal(generateHint('aabb'), '1-1'); // does not count duplicate thus 1-1
     });
 
   });
