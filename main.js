650c3b1437b319bbcb2f017f0c7840a07f4ecc82
TheYingineer <ying.yan.huang87@gmail.com>
2022-05-20 21:48:21 -0500

    ready to submit

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-const printBoard = () =>  {
+const printBoard = () => {
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
 }
 
-const generateSolution = () =>  {
+const generateSolution = () => {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
@@ -32,14 +32,14 @@ const getRandomInt = (min, max) => {
  * Spec 2.1 - Split up the solution and guess: In generateHint(), create variables solutionArray and guessArray that each split up passed in arguments, [.split](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/splitting on '' (empty string).
 Spec 2.2 - Determine correct "letter-locations": Create a variable correctLetterLocations and set it to 0. This variable will record how many correct "letter-locations" were guessed. For instance, a guess of aabc against a solution of deba would yield one correct "letter-location" (b). In a for loop, iterate over the solutionArray, comparing each index of solutionArray against the same index of guessArray. If the item matches, increment correctLetterLocations, and set that index in solutionArray to null.
  */
-const generateHint = (guess) =>  {
+const generateHint = (guess) => {
   let solutionArray = solution.split("")
   let guessArray = guess.split("")
   console.log(solutionArray)
   console.log(guessArray)
   let correctLetterLocations = 0
-  for(let i=0; i<solutionArray.length; i++) {
-    if(solutionArray[i] === guessArray[i]){
+  for (let i = 0; i < solutionArray.length; i++) {
+    if (solutionArray[i] === guessArray[i]) {
       correctLetterLocations = correctLetterLocations + 1
       solutionArray[i] = 0
       guessArray[i] = 1
@@ -51,33 +51,34 @@ const generateHint = (guess) =>  {
   // Spec 2.4 - return hint string: Optionally, you can use the colors package, return a string that prints out the hints you generated, with correctLetterLocations being red, correctLetters being white, and separated by a hyphen. > (NOTE: If you choose to use this color package, only console.log the result. If you return the result your program will fail the tests.)
 
   let correctLetters = 0
-for(let j = 0; j<guessArray.length; j++){
-  let targetIndex = guessArray.indexOf(solutionArray[j])
-  if(targetIndex>-1){
-    correctLetters = correctLetters+1
+  for (let j = 0; j < guessArray.length; j++) {
+    let targetIndex = guessArray.indexOf(solutionArray[j])
+    if (targetIndex > -1) {
+      console.log('yes')
+      correctLetters = correctLetters + 1
 
-    //mutate both solution and guess array so that handle duplicates properly
-    solutionArray[j] = 0 //Ceaser
-    guessArray[targetIndex] = 1 //Ceaser
+      //mutate both solution and guess array so that handle duplicates properly
+      solutionArray[j] = 0 //Ceaser
+      guessArray[targetIndex] = 1 //Ceaser
+    }
   }
+  console.log(correctLetterLocations + "-" + correctLetters)
+  return correctLetterLocations + "-" + correctLetters
 }
-return correctLetterLocations + "-" + correctLetters
-}
-
 // Spec 3 - Add guess and hint to the board: Define a variable called hint that collects the returned value of generateHint(guess). .push the guess and the hint (as a combined string) into the board.
 
 // Spec 4 - End the game: After 10 incorrect guesses, if the board length equals 10, return 'You ran out of turns! The solution was' and the solution. Otherwise, return 'Guess again.'.
 
 
 const mastermind = (guess) => {
-  solution = 'dced';
+  solution = 'abcd';
 
-  if(board.length === 10){
-    return console.log( "You ran out of turns! The solution was " + solution)
+  if (board.length === 10) {
+    return console.log("You ran out of turns! The solution was " + solution)
   }
 
 
-  if(guess === solution){
+  if (guess === solution) {
     return "You guessed it!"
   }
   else {
@@ -90,7 +91,7 @@ const mastermind = (guess) => {
 }
 
 
-const getPrompt = () =>  {
+const getPrompt = () => {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
     printBoard();
